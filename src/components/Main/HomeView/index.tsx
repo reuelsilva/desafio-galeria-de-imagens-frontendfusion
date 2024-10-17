c8d393c621088c0f8bd30ee9f5f585930e8a403e
reuelsilva <reuelsilva19@gmail.com>
2024-10-17 19:54:25 -0300

    feat: adiciona o componente HomeView
    - utiliza o custom hook useImages para buscar imagens da API Lorem Picsum
    - integra o contexto gallery-context para gerenciar a paginação de imagens
    - implementa a funcionalidade de modal para visualização ampliada de imagens
    - cria botão "Carregar mais" para carregar mais imagens ao clicar
    - adiciona estilo responsivo para a exibição das imagens em grade

---
+++ src/components/Main/HomeView/index.tsx
@@ -0,0 +1,46 @@
+import React, { Fragment, useContext, useState } from "react"
+import useImages from "../../../hooks/useImages"
+import { imageProps } from "../../../types/image-types";
+import { Context } from "../../../contexts/gallery-context";
+import Modal from "../Modal";
+
+function HomeView(): JSX.Element {
+
+    const { images } = useImages();
+    const { setPage } = useContext(Context)
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedImg, setSelectedImg] = useState<imageProps>({
+        author:"", 
+        download_url: "", 
+        height:0, id:0, 
+        url:"", 
+        width:0
+    })
+
+    return (
+        <Fragment>
+            <div className="flex flex-col items-center gap-4">
+                <div className="grid sm:grid-cols-[repeat(2,_300px)] lg:grid-cols-[repeat(3,_300px)] gap-2 justify-center">
+                    {
+                        images.map((image: imageProps, id: number) => {
+                            return (
+                                <div key={id} className="w-[300px] h-[215px] overflow-hidden rounded-lg sm:cursor-pointer transition-all hover:brightness-50" onClick={() => {
+                                    setIsModalOpen(prev => !prev);
+                                    setSelectedImg(image);
+                                }}>
+                                    <img className="h-[215px] w-full transition-all hover:scale-125" src={image.download_url} alt={`Imagem #${image.id}`} />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <button className="bg-color-02 text-white text-sm font-medium p-2 rounded-lg cursor-pointer" onClick={() => setPage(prevPage => prevPage + 1)}>Carregar mais</button>
+            </div>
+            {
+                isModalOpen && <Modal image={selectedImg} setIsModal={setIsModalOpen} isModalOpen={isModalOpen} />
+            }
+        </Fragment>
+    )
+}
+
+export default HomeView
